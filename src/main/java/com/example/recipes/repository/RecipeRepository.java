2ee54467e845c07e52f1d956a8b3445aaca70bc9
Anna Zahrebelna <annzagrebelnaya@gmail.com>
2024-07-23 17:50:20 +0200

    Remove PagingAndSortingRepository

--- src/main/java/com/example/recipes/repository/RecipeRepository.java
+++ src/main/java/com/example/recipes/repository/RecipeRepository.java
@@ -3,7 +3,6 @@ package com.example.recipes.repository;
 import com.example.recipes.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long>, PagingAndSortingRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe> {
 }
